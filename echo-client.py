ccb2994625be7f017da3e8bcbd366c06b6bbb951
Adrian Wijaya <adrianwijaya100@gmail.com>
2020-10-19 00:48:54 +0700

    Minimal requirement + multi jobs sent

--- echo-client.py
+++ echo-client.py
@@ -1,14 +1,50 @@
 #!/usr/bin/env python3
 
-import socket
+import socket, threading
 
-# HOST = '127.0.0.1'
-HOST = '54.157.4.185'
+HOST = '127.0.0.1'
+# HOST = '54.157.4.185'
 PORT = 65432
+queue_jobs = []
+MENU_MSG = ["Select command you want to choose ", "1. Type send <program path> <set of arguments>",
+        "2. Type else to quit program"]
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+def start_connection(path_program, arg):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        with open(path_program, "rb") as f:
+            data = f.read()
+            s.sendall(data)
+            print("data terkirim")
+            s.shutdown(1)
+        output = s.recv(1024)
+        print("Received jobs result from {} :\n{}".format(path_program, output.decode()))
 
-print('Received', repr(data))
+def send_job():
+    global queue_jobs
+    while True:
+        if len(queue_jobs) != 0:
+            job = queue_jobs[0]
+            queue_jobs = queue_jobs[1:]
+            start_connection(job[0], job[1])
+
+def wait_input():
+    while True:
+        for msg in MENU_MSG:
+            print(msg)
+        
+        cmd = input().split()
+        if cmd[0] == "send":
+            path_program, arg = cmd[1], cmd[2:]
+            queue_jobs.append([path_program, arg])
+        else:
+            # handle quit program (not solved)
+            sys.exit()
+
+if __name__ == "__main__" :
+    # Todo handle argument
+    wait = threading.Thread(target=wait_input, args=())
+    wait.start()
+    send = threading.Thread(target=send_job, args=())
+    send.start()
+
--- echo-server.py
+++ echo-server.py
@@ -1,22 +1,44 @@
 #!/usr/bin/env python3
 
-import socket
+import socket, os
 
 HOST = ''
 PORT = 65432
 
 print("Waiting for connection")
 
+def write_program(conn):
+    while True:
+        # print("masih menunggu")
+        data = conn.recv(1024)
+        if not data:
+            break
+        else:
+            with open("program-test", "ab") as f:
+                f.write(data)
+
+def run_program():
+    os.system("chmod +x program-test")
+    os.system("./program-test > program-test.out")
+
+def send_output(conn):
+    with open("program-test.out", "rb") as f:
+         data = f.read()
+         print(repr(data))
+         conn.sendall(data)
+
 # Socketnya pakai IPv4 dan TCP
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(conn)
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+while True:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(conn)
+            print('Connected by', addr)
+
+            write_program(conn)
+            run_program()
+            send_output(conn)
+
+            os.system("rm program-test program-test.out") # clean program
